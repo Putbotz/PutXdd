afae06039169573ba6b5fc8c47f14939d7677872
Rulsem <112336609+Rulsem@users.noreply.github.com>
2022-09-09 14:54:35 +0800

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `Rul single nk ayang bng ╭∩╮(-_-)╭∩╮`
+let info = `Rul nk Kyra bang hehe ╭∩╮(-_-)╭∩╮`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 /*const message = {
