a623c167ba95699f88a7b56d567fd6bbd1f6ed4e
Putbotz <96361236+Putbotz@users.noreply.github.com>
2023-01-02 14:55:02 +0800

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `Amyy onzzz ! (≧∀≦)`
+let info = `teahcumel🐢`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 /*const message = {
