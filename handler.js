2555228d488b131f2f76249066debe379635b480
Putbotz <96361236+Putbotz@users.noreply.github.com>
2023-03-06 07:53:08 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -332,7 +332,7 @@ export async function handler(chatUpdate) {
                     sBye: '',
                     sPromote: '',
                     sDemote: '',
-                    delete: true,
+                    delete: false,
                     antiLink: false,
                     sticker: false,
                     viewonce: true,
