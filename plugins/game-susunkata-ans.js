4bd094a68946bf46e5fa3f6dd69ab6f450e85aa7
TengkuShafiq <132145610+TengkuShafiq@users.noreply.github.com>
2023-05-05 10:06:00 +0800

    Update game-susunkata-ans.js

--- plugins/game-susunkata-ans.js
+++ plugins/game-susunkata-ans.js
@@ -5,7 +5,7 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*suska/i.test(m.quoted.text)) return !0
     this.susunkata = this.susunkata ? this.susunkata : {}
-    if (!(id in this.susunkata)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.susunkata)) return m.reply('Soalan itu telah berakhir')
     if (m.quoted.id == this.susunkata[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
@@ -15,7 +15,7 @@ handler.before = async function (m) {
             m.reply(`*Benar!*\n+${this.susunkata[id][2]} Money\n+1 Limit`)
             clearTimeout(this.susunkata[id][3])
             delete this.susunkata[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Sikit Lagi!*`)
         else m.reply(`*Salah!*`)
     }
     return !0
