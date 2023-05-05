4ad7b5aaee336bbedf734b4185d94ae4a24a0b35
TengkuShafiq <132145610+TengkuShafiq@users.noreply.github.com>
2023-05-05 09:31:56 +0800

    Update fun-kapankah2.js

--- plugins/fun-kapankah2.js
+++ plugins/fun-kapankah2.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, command, text }) => conn.reply(m.chat, `
 *Pertanyaan:* ${command} ${text}
-*Jawaban:* ${(10).getRandom()} ${['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'].getRandom()} lagi ...
+*Jawaban:* ${(10).getRandom()} ${['saat', 'minit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'].getRandom()} lagi ...
   `.trim(), m, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
 
-handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <text>?')
+handler.help = ['', 'kah'].map(v => 'bila' + v + ' <text>?')
 handler.tags = ['kerang', 'fun']
-handler.command = /^kapan(kah)?$/i
+handler.command = /^bila(kah)?$/i
 
 export default handler
