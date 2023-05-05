9802c6b167630a3ffec22a8b2f84329f2ef2ac50
TengkuShafiq <132145610+TengkuShafiq@users.noreply.github.com>
2023-05-05 19:50:08 +0800

    Update fun-apakah2.js

--- plugins/fun-apakah2.js
+++ plugins/fun-apakah2.js
@@ -1,13 +1,12 @@
-let handler = async (m) => m.reply(`
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${['Ya', 'Mungkin lah kot', 'Mungkin', 'Entah aku rasa tak', 'Tidak', 'Tak tahu la dekat dia'].getRandom()}
+let handler = async (m, { command, text }) => m.reply(`
+*Pertanyaan:* ${command} ${text}
+*Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
 
 handler.help = ['apakah <teks>?']
 handler.tags = ['kerang', 'fun']
-handler.customPrefix = /(\?$)/
 handler.command = /^apakah$/i
 
 export default handler
