53ec22f4b932e91195dfd1e96f9c18f32f864819
TengkuShafiq <132145610+TengkuShafiq@users.noreply.github.com>
2023-05-05 09:57:33 +0800

    Update fun-apakah.js

--- plugins/fun-apakah.js
+++ plugins/fun-apakah.js
@@ -1,6 +1,6 @@
 let handler = async (m) => m.reply(`
 *Pertanyaan:* ${m.text}
-*Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
+*Jawaban:* ${['Ya', 'Mungkin la kot', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin,'Betul la tu','Haah betul la','Mana aku tau dia'].getRandom()}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
